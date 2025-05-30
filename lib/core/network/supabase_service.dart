518feb313f7fbc257583a86a81e7aab81887cbf7
tosh <shellcodegroup@gmail.com>
2025-05-30 14:59:26 +0300

    Refactor SupabaseService to use environment configuration for base URL and API key
    - Replaced hardcoded Supabase base URL and API key with values from EnvConfig for improved security and flexibility.

--- lib/core/network/supabase_service.dart
+++ lib/core/network/supabase_service.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:visitstracker/core/config/env_config.dart';
 
 class SupabaseService {
-  static const String _baseUrl =
-      'https://kqgbftwsodpttpqgqnbh.supabase.co/rest/v1';
-  static const String _apiKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxZ2JmdHdzb2RwdHRwcWdxbmJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5ODk5OTksImV4cCI6MjA2MTU2NTk5OX0.rwJSY4bJaNdB8jDn3YJJu_gKtznzm-dUKQb4OvRtP6c';
-
+  static const String _baseUrl = EnvConfig.supabaseUrl;
+  static const String _apiKey = EnvConfig.supabaseKey;
   final http.Client _client;
 
   SupabaseService(this._client);
